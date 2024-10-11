6df1d2c815cecb39dc3c2e952c44c29445b18ca6
nxkoi <danilo.engc@gmail.com>
2024-10-11 09:23:40 -0300

    Update remove_kubernets.sh

--- remove_kubernets.sh
+++ remove_kubernets.sh
@@ -4,14 +4,18 @@
 echo "Resetando o kubeadm..."
 sudo kubeadm reset -f
 
-# Parar os serviços do kubelet e docker
-echo "Parando os serviços kubelet e docker..."
+# Parar o serviço do kubelet e containerd
+echo "Parando os serviços kubelet e containerd..."
 sudo systemctl stop kubelet
-sudo systemctl stop docker
+sudo systemctl stop containerd
+
+# Desmarcar os pacotes retidos
+echo "Liberando pacotes mantidos do Kubernetes..."
+sudo apt-mark unhold kubeadm kubectl kubelet
 
 # Remover pacotes do Kubernetes
 echo "Removendo pacotes do Kubernetes..."
-sudo apt-get purge -y kubeadm kubectl kubelet kubernetes-cni kube*
+sudo apt-get purge -y kubeadm kubectl kubelet
 sudo apt-get autoremove -y
 
 # Limpar diretórios residuais do Kubernetes
@@ -19,16 +23,15 @@ echo "Limpando diretórios residuais..."
 sudo rm -rf ~/.kube
 sudo rm -rf /etc/kubernetes
 sudo rm -rf /var/lib/kubelet
-sudo rm -rf /var/lib/etcd
 sudo rm -rf /etc/cni
 sudo rm -rf /opt/cni/bin
 
-# Limpar arquivos do Docker relacionados ao Kubernetes
-echo "Removendo configurações e dados do Docker relacionados ao Kubernetes..."
-sudo rm -rf /var/lib/docker
+# Limpar arquivos do containerd relacionados ao Kubernetes
+echo "Removendo configurações e dados do containerd relacionados ao Kubernetes..."
+sudo rm -rf /var/lib/containerd
 
-# Reiniciar o sistema para garantir que todas as mudanças sejam aplicadas
-echo "Reiniciando o sistema..."
-sudo systemctl restart docker
+# Reiniciar o serviço containerd
+echo "Reiniciando o serviço containerd..."
+sudo systemctl restart containerd
 
 echo "Remoção completa do Kubernetes finalizada!"
